3c09fe37dd3762c950a6ff5d89d75696b9dd56c2
manoj1202 <manoj.appalaneni@gmail.com>
2018-08-18 15:44:13 -0500

    adding Passport for authentication

--- modles/user.js
+++ modles/user.js
@@ -30,6 +30,16 @@ var User = module.exports = mongoose.model('User',UserSchema);
 module.exports.getUserById =function(id,callback){
     User.findById(id,callback)
 }
+module.exports.getUserByUsername = function(username,callback){
+    var query = {username : username};
+    User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatepassword, hash, callback){
+    bcrypt.compare(candidatepassword, hash, function(err, isMatch) {
+        callback(null, isMatch)
+    });
+}
 
 module.exports.createUser = function(newUser, callback){
     bcrypt.genSalt(10, function(err, salt) {
