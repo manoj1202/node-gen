0925dd9156ac5c7ad8ae89c94b23f90643740dd5
manoj1202 <manoj.appalaneni@gmail.com>
2018-08-18 09:54:56 -0500

    creating Register page

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
+var passport = require("passport");
+var ExpressValidator = require("express-validator");
+var LocalStrategy = require("passport-local").strategy;
+var multer = require("multer");
+var upload = multer({dest: "./uploads"});
+var flash = require("connect-flash");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var db = mongoose.connection;
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,11 +25,45 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Hnadle Sessions
+app.use(session({
+  secret:"secret",
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Validator
+app.use(ExpressValidator({
+  errorFormatter: function(param, msg,value){
+    var namespace = param.split(".")
+    , root = namespace.shift()
+    , formpara = root;
+    while(namespace.length){
+      formParam += "[" + namespace.shift() + "]";
+    }
+    return{
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
